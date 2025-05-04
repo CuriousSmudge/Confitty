a4b7961f96e5062d8ce714fb88acd0ae3a060553
CuriousSmudge <curioussmudge@gmail.com>
2025-05-05 08:49:27 +1000

    Made the Terminal
    Added Terminal Class that can emulate the following functions:
    - accesses the shell and starts it
    - hides the emulation from the shell
    - enters raw mode and reverts upon exit
    - processes simple commands

---
+++ confitty.py
@@ -0,0 +1,86 @@
+import pygame as pg
+import sys
+import os
+import pty
+import termios
+import tty
+import select
+
+pg.init()
+BUFFER_READ_BYTES = 1024
+BACKGROUND = (30, 30, 46)
+
+
+class Terminal:
+    def __init__(self):
+        self.shellPath = self.get_shell()
+        self.initiate_shell(self.shellPath)
+
+    def get_shell(self):
+        shellPath = os.getenv("SHELL")
+        if shellPath is None:
+            print("ERROR: Shell path not found - Did you install a shell?")
+            raise Exception
+        else:
+            return shellPath
+
+    def initiate_shell(self, shellPath):
+        self.processID, self.master = pty.fork()
+
+        if self.processID == 0:
+            print("Shell Initiated")
+            os.execvp(shellPath, [shellPath])
+
+    def enter_raw_mode(self):
+        # Backs up the terminal configuration so emulator can be reverted
+        print("Entering Raw Mode")
+        self.backup = termios.tcgetattr(sys.stdin)
+        print("Terminal backed up successfully")
+        tty.setraw(sys.stdin.fileno())
+
+    def check_input(self, rlist):
+        # Reads from the user input to find stdin
+        if sys.stdin in rlist:
+            inputs = os.read(sys.stdin.fileno(), BUFFER_READ_BYTES)
+            if not inputs:
+                return
+            os.write(self.master, inputs)
+
+    def check_output(self, rlist):
+        # Reads from the user to find stdout (returned from master)
+        if self.master in rlist:
+            output = os.read(self.master, BUFFER_READ_BYTES)
+            if not output:
+                return
+            os.write(sys.stdout.fileno(), output)
+
+    def run(self):
+        if self.processID == 0:
+            # pty.fork(), Slave/child process always has processID == 0
+            print("Slave refused access")
+            return
+        self.enter_raw_mode()
+
+        try:
+            while True:
+                # Get the inputs and outputs and then check them
+                rlist, _, _ = select.select([sys.stdin, self.master], [], [])
+                self.check_input(rlist)
+                self.check_output(rlist)
+        finally:
+            print("Restoring...")
+            self.restore_terminal()
+            print("Terminal restored. Quitting...")
+
+    def restore_terminal(self):
+        if self.backup:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.backup)
+
+
+def main():
+    Term = Terminal()
+    Term.run()
+
+
+if __name__ == "__main__":
+    main()
