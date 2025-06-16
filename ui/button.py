07e521a58a9f8015d21e8ae24e1df37b39af3a1a
CuriousSmudge <curioussmudge@gmail.com>
2025-06-16 12:46:01 +1000

    Add settings dialoug with toggling dark_mode

--- ui/button.py
+++ ui/button.py
@@ -1,39 +1,78 @@
-import pygame as pg
-from ui import window
+import pyray as pr
+from ui import con
+from dataclasses import dataclass
 
-# Colours
-BUTTON = (24, 24, 37)
-BUTTON_CLICK = (17, 17, 27)
-TEXT = (203, 166, 247)
 
+# self.width = con.WIDTH // self.character_size[0]
+# self.height = con.HEIGHT // self.character_size[1]
 
 class Button:
-    mousePos = pg.mouse.get_pos()
+    mousePos = pr.get_mouse_position()
 
-    def __init__(self, shape: pg.Rect, text: str):
-        self.shape = shape
+    def __init__(self, rect: tuple, text, color=con.MANTLE, text_col=con.MAUVE):
+        # pr.Rectangle = (x, y, width, height)
+        self.rect = rect
         self.text = text
-        # self.textSize =
-        self.colour = BUTTON
-        self.click = BUTTON_CLICK
-        self.textColour = TEXT
-        self.fontObject = pg.font.Font()
-        self.fontObject.align = pg.FONT_CENTER
+        self.colour = color
+        self.textColour = text_col
 
-    def is_clicked(self, mousePos) -> bool:
-        if pg.mouse.get_just_released()[0] and self.shape.collidepoint(mousePos):
-            print("Button clicked!")
+    def render_button(self):
+        pr.draw_rectangle_rounded(self.rect, 4, 1, self.colour)
+        pr.draw_text(self.text, self.rect[0]+24, self.rect[1]+12, 16, self.textColour)
+
+    def is_clicked(self, mousePos):
+        if pr.check_collision_circle_rec(mousePos, 0.1, self.rect) and pr.is_mouse_button_pressed(pr.MOUSE_BUTTON_LEFT):
+            print("Settings Menu Clicked")
+            # self.colour = con.CRUST
             return True
         else:
             return False
 
-    def render_button(self):
-        pg.draw.rect(screen, self.colour, self.shape)
-        textSurface = self.render_text()
-        textX = self.shape.width // 2 + self.shape.x - textSurface.width // 2
-        textY = self.shape.height // 2 + self.shape.y - textSurface.height // 2
-        screen.blit(textSurface, (textX, textY))
 
-    def render_text(self):
-        textSurface = self.fontObject.render(self.text, True, TEXT)
-        return textSurface
+
+# TODO
+# - [ ] Button takes in callback
+# - [ ] Button should have update method for each frame
+# - [ ]
+# - [ ]
+# - [ ]
+
+# # import pygame as pg
+# from ui import window
+
+# # Colours
+# BUTTON = (24, 24, 37)
+# BUTTON_CLICK = (17, 17, 27)
+# TEXT = (203, 166, 247)
+
+
+# class Button:
+#     mousePos = pg.mouse.get_pos()
+
+#     def __init__(self, shape: pg.Rect, text: str):
+#         self.shape = shape
+#         self.text = text
+#         # self.textSize =
+#         self.colour = BUTTON
+#         self.click = BUTTON_CLICK
+#         self.textColour = TEXT
+#         self.fontObject = pg.font.Font()
+#         self.fontObject.align = pg.FONT_CENTER
+
+#     def is_clicked(self, mousePos) -> bool:
+#         if pg.mouse.get_just_released()[0] and self.shape.collidepoint(mousePos):
+#             print("Button clicked!")
+#             return True
+#         else:
+#             return False
+
+#     def render_button(self):
+#         pg.draw.rect(screen, self.colour, self.shape)
+#         textSurface = self.render_text()
+#         textX = self.shape.width // 2 + self.shape.x - textSurface.width // 2
+#         textY = self.shape.height // 2 + self.shape.y - textSurface.height // 2
+#         screen.blit(textSurface, (textX, textY))
+
+#     def render_text(self):
+#         textSurface = self.fontObject.render(self.text, True, TEXT)
+#         return textSurface
--- ui/window.py
+++ ui/window.py
@@ -1,7 +1,9 @@
 import pyray as pr
 from ui import con
+from ui import button
 from processing import terminal
 from dataclasses import dataclass
+import configparser
 
 
 @dataclass
@@ -21,6 +23,19 @@ class Window:
         pr.set_target_fps(60)
         self.term = terminal.Terminal()
 
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+
+        # Lambda is so cool :)
+        tf = lambda s: True if s.lower() == "true" else False
+
+        self.in_dark_mode: bool = tf(self.config['config']["dark_mode"])
+        print(self.config['config']["dark_mode"])
+        print(self.in_dark_mode)
+
+        self.update_colours()
+
+
         self.character_size = (10, 10)
         self.width = con.WIDTH // self.character_size[0]
         self.height = con.HEIGHT // self.character_size[1]
@@ -33,12 +48,17 @@ class Window:
         self.inFE = False
         self.ESCBuffer = bytearray()
         print("Terminal has started in raylib")
+
+        self.settings = button.Button((con.WIDTH-120, con.HEIGHT-440, 110, 40), "Settings", self.second_background, self.text_col)
         self.run()
 
     def run(self):
         while not pr.window_should_close():
             pr.begin_drawing()
-            pr.clear_background(con.BASE)
+            pr.clear_background(self.background_col)
+            self.settings.render_button()
+            while self.settings.is_clicked(pr.get_mouse_position()):
+                self.run_settings() 
             self.run_terminal()
             self.display_output()
             self.get_inputs()
@@ -108,25 +128,32 @@ class Window:
 
     @staticmethod
     def is_shift_pressed():
-        left, right = pr.is_key_down(pr.KEY_LEFT_SHIFT), pr.is_key_down(pr.KEY_RIGHT_SHIFT)
+        left, right = pr.is_key_down(pr.KEY_LEFT_SHIFT), pr.is_key_down(
+            pr.KEY_RIGHT_SHIFT
+        )
         if left == True or right == True:
             return True
-        else: return False
-    
+        else:
+            return False
+
     @staticmethod
     def is_ctrl_pressed():
-        left, right = pr.is_key_down(pr.KEY_LEFT_CONTROL), pr.is_key_down(pr.KEY_RIGHT_SHIFT)
+        left, right = pr.is_key_down(pr.KEY_LEFT_CONTROL), pr.is_key_down(
+            pr.KEY_RIGHT_SHIFT
+        )
         if left == True or right == True:
             return True
-        else: return False
-    
+        else:
+            return False
+
     @staticmethod
     def is_alt_pressed():
         left, right = pr.is_key_down(pr.KEY_LEFT_ALT), pr.is_key_down(pr.KEY_RIGHT_ALT)
         if left == True or right == True:
             return True
-        else: return False
-    
+        else:
+            return False
+
     def get_modifier_state(self) -> tuple:
         return (self.is_shift_pressed(), self.is_ctrl_pressed(), self.is_alt_pressed())
 
@@ -135,7 +162,7 @@ class Window:
         draw_pos = [0, 0]
         for line in self.canvas:
             for cell in line:
-                pr.draw_text(cell.text, draw_pos[1], draw_pos[0], 12, con.TEXT)
+                pr.draw_text(cell.text, draw_pos[1], draw_pos[0], 12, self.text_col)
                 draw_pos[0] += self.character_size[0]
 
             draw_pos[0] = 0
@@ -151,7 +178,7 @@ class Window:
             print("In CSI/C2 Escape Sequence")
             self.inCSI = True
             return
-        if self.inCSI: # Handle CSI Sequences
+        if self.inCSI:  # Handle CSI Sequences
             if 0x40 <= byteVal <= 0x7E:
                 print(f"Ending CSI Sequence with Final Byte: 0x{byte.hex()}")
                 self.handle_csi_sequence()
@@ -159,44 +186,98 @@ class Window:
                 return
             return
 
-        # C1 Control Codes 
-        if len(self.ESCBuffer) == 1 and 0x40 <= byteVal <= 0x5F: # byte > b"\x40" and byte < b"\x5f":
+        # C1 Control Codes
+        if (
+            len(self.ESCBuffer) == 1 and 0x40 <= byteVal <= 0x5F
+        ):  # byte > b"\x40" and byte < b"\x5f":
             # https://en.wikipedia.org/wiki/C0_and_C1_control_codes
             print(f"C1 Control Cdoe: ESC{chr(byteVal)}")
             self.inFE = True
             self.end_escape_sequence()
-        
+
         # C0 Single Character Escape Sequences
-        if len(self.ESCBuffer) == 1 and (0x60 <= byteVal <= 0x7E or 0x20 <= byteVal <= 0x2F):
+        if len(self.ESCBuffer) == 1 and (
+            0x60 <= byteVal <= 0x7E or 0x20 <= byteVal <= 0x2F
+        ):
             print(f"C0 Single Character Escape: ESC{chr(byte(val))}")
             self.end_escape_sequence()
             return
 
     def handle_csi_sequence(self):
-        sequence = self.ESCBuffer.decode('utf-8', errors='ignore')
+        sequence = self.ESCBuffer.decode("utf-8", errors="ignore")
         print(f"Handling CSI Sequence: ESC{sequence}")
-        final_byte = sequence[-1] if sequence else ''
+        final_byte = sequence[-1] if sequence else ""
 
         match final_byte:
-            case 'K': # Erase in Line
+            case "K":  # Erase in Line
                 print("Erase in Line (K)")
-                for x in range (self.cursor.x, self.width):
+                for x in range(self.cursor.x, self.width):
                     self.canvas[x][self.cursor.y].text = ""
-            case 'J': # Erase in Display
+            case "J":  # Erase in Display
                 print("Erase in Display (J)")
                 for x in range(self.width):
                     for y in range(self.height):
                         self.canvas[x][y].text = ""
-            case 'H'|'f': # Cursor Position
+            case "H" | "f":  # Cursor Position
                 print(f"Cursor Position ({final_byte})")
                 self.cursor.x = 0
                 self.cursor.y = 0
             case _:
                 print(f"Unhandled CSI Sequence: {final_byte}")
-    
+
     def end_escape_sequence(self):
         self.InEscapeSequence = False
         self.inCSI = False
         self.inFE = False
         self.ESCBuffer = bytearray()
         print("Escape Sequence Ended")
+
+    def update_colours(self):
+        if self.in_dark_mode:
+            self.background_col = con.BASE
+            self.second_background = con.MANTLE
+            self.text_col = con.TEXT
+        else:
+            self.background_col = con.TEXT
+            self.second_background = con.OVERLAY2
+            self.text_col = con.BASE
+
+    def run_settings(self):
+        print("Creating new Window")
+        pr.init_window(con.WIDTH-80, con.HEIGHT-40, "Settings")
+        print("New Window Created")
+        settings = button.Button((120, 140, 200, 40), "Toggle Dark mode", self.second_background, self.text_col)
+        
+        close_butt = button.Button((10, 20, 40, 40), "X", self.second_background, self.text_col)
+
+        def toggle_dark_mode(butt):
+            if self.in_dark_mode:
+                self.in_dark_mode = False
+            else:
+                self.in_dark_mode = True
+            
+            self.update_colours()
+            # Write the new config to file
+            self.config['config']['dark_mode'] = str(self.in_dark_mode)
+            with open('config.ini', 'w') as f:
+                self.config.write(f)
+
+        should_exit = False
+        while not should_exit:
+            pr.begin_drawing()
+            pr.clear_background(self.background_col)
+            # pr.draw_text("Settings", 24, 24, 48, self.text_col)
+
+            settings.render_button()
+            if settings.is_clicked(pr.get_mouse_position()):
+                toggle_dark_mode(settings)
+
+            close_butt.render_button()
+            if close_butt.is_clicked(pr.get_mouse_position()):
+                should_exit = True
+
+            pr.end_drawing()
+
+        pr.close_window()
+        a = Window()
+        a.run()
